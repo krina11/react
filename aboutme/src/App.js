a8f5dd85ece909defbc5fa3560211bf09f5d4adc
krina11 <47170603+krina11@users.noreply.github.com>
2022-01-31 12:32:31 -0800

    add scss to react

--- aboutme/src/App.js
+++ aboutme/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './App.scss';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="app-text">
+          Edit here <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
